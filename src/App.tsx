7f34fb9d5389c9b18f158c5784996c1664e0b5d6
Tatyana <tkoshikova@gmail.com>
2023-07-08 05:40:33 +0700

    Инициализация приложения, загрузка основных компонент

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Questions from 'components/Questions';
+
+const App = () => {
+
+    return (
+        <div className="app">
+            <Questions/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+import App from 'App';
+
+const root = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </Provider>
+)
+
+ReactDOM.render(
+    root,
+    document.getElementById('root')
+);
