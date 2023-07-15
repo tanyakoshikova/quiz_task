926c639b0e6d2be496470e44408c29aa6196a5ab
Tatyana <tkoshikova@gmail.com>
2023-07-15 13:53:40 +0700

    Тестовое "Квиз"

---
+++ frontend/src/pages/Main/Main/Login/Login.tsx
@@ -0,0 +1,45 @@
+import React, { FormEvent, useState } from "react";
+import {loginUser} from "store/auth/actionCreators";
+import {useAppDispatch} from "store";
+
+const Login = () => {
+
+    const dispatch = useAppDispatch();
+
+    const [login, setLogin] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        dispatch(loginUser({ login, password }));
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="login">Login:</label>
+                    <input
+                        name="login"
+                        type="text"
+                        value={login}
+                        onChange={(e) => setLogin(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
