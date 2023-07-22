380f0eb978030b219248e3e07dd6ae4249e19912
Tatyana <tkoshikova@gmail.com>
2023-07-22 16:34:19 +0700

    refactoring backend part

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,6 @@ const PORT = config.get('serverPort')
 const authRouter = require("./routes/auth.routes")
 const corsMiddleware = require('./middleware/cors.middleware')
 
-
 app.use(corsMiddleware)
 app.use(express.json())
 app.use("/api/auth", authRouter);
